f008db59ddb647b2faa1dbd73eb49380b87a30d7
willgardner9 <willgardner9@gmail.com>
2021-09-12 22:36:43 +0100

    Inject t9 service to controller

--- server/src/t9/t9.controller.ts
+++ server/src/t9/t9.controller.ts
@@ -1,4 +1,7 @@
 import { Controller } from '@nestjs/common';
+import { T9Service } from './t9.service';
 
 @Controller('t9')
-export class T9Controller {}
+export class T9Controller {
+  constructor(private t9Service: T9Service) {}
+}

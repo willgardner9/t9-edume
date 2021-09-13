5db520b0cd6f20b60ea86ea04f426a6f4c2ab294
willgardner9 <willgardner9@gmail.com>
2021-09-13 15:22:07 +0100

    Added tests for inputs including 0 or 1

--- server/src/t9/t9.service.spec.ts
+++ server/src/t9/t9.service.spec.ts
@@ -1,8 +1,16 @@
 import { Test } from '@nestjs/testing';
 import { T9Service } from './t9.service';
 
-const mockConvertNumbersDto = {
+const mockConvertNumbersDto23 = {
   numbers: '23',
+};
+
+const mockConvertNumbersDto230 = {
+  numbers: '230',
+};
+
+const mockConvertNumbersDto231 = {
+  numbers: '231',
 };
 
 describe('t9Service', () => {
@@ -18,17 +26,29 @@ describe('t9Service', () => {
 
   describe('convertT9NumbersToText', () => {
     it('converts t9 input numbers to text', () => {
-      expect(t9Service.convertT9NumbersToText(mockConvertNumbersDto)).toEqual({
-        '0': 'ad',
-        '1': 'ae',
-        '2': 'af',
-        '3': 'bd',
-        '4': 'be',
-        '5': 'bf',
-        '6': 'cd',
-        '7': 'ce',
-        '8': 'cf',
-      });
+      expect(t9Service.convertT9NumbersToText(mockConvertNumbersDto23)).toEqual(
+        {
+          '0': 'ad',
+          '1': 'ae',
+          '2': 'af',
+          '3': 'bd',
+          '4': 'be',
+          '5': 'bf',
+          '6': 'cd',
+          '7': 'ce',
+          '8': 'cf',
+        },
+      );
+    });
+    it('throws an error when inputting a 0', () => {
+      expect(() =>
+        t9Service.convertT9NumbersToText(mockConvertNumbersDto230),
+      ).toThrowError();
+    });
+    it('throws an error when inputting a 1', () => {
+      expect(() =>
+        t9Service.convertT9NumbersToText(mockConvertNumbersDto231),
+      ).toThrowError();
     });
   });
 });

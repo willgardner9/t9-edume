b1420518dfd67e821bfc48bc5cbf29e7005efc83
willgardner9 <willgardner9@gmail.com>
2021-09-13 20:21:32 +0100

    Basic app setup: get data from user, call API, display response in UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,29 @@
 import "./App.css";
+import {useState, useEffect} from "react";
+import axios from "axios";
 
 function App() {
-  return <></>;
+  const [t9Combinations, setT9Combinations] = useState();
+  const [numbers, setNumbers] = useState("44");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (!numbers) return;
+      const result = await axios.post("http://localhost:9000/t9", {numbers});
+      setT9Combinations(result.data);
+    };
+
+    fetchData();
+  }, [numbers]);
+
+  return (
+    <>
+      <form>
+        <input type="number" onChange={(e) => setNumbers(e.target.value)} />
+      </form>
+      {JSON.stringify(t9Combinations, null, 2)}
+    </>
+  );
 }
 
 export default App;

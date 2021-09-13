b0a4a23c61eefd543af2d4ef65582edfb39c7a3c
willgardner9 <willgardner9@gmail.com>
2021-09-13 20:33:38 +0100

    Basic error handling and messages on client

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,20 @@ import {useState, useEffect} from "react";
 import axios from "axios";
 
 function App() {
+  const [errorMessage, setErrorMessage] = useState();
   const [t9Combinations, setT9Combinations] = useState();
   const [numbers, setNumbers] = useState("44");
 
   useEffect(() => {
     const fetchData = async () => {
       if (!numbers) return;
+      if (numbers.includes("0") || numbers.includes("1")) {
+        setNumbers();
+        return setErrorMessage(`T9 doesn't include 0's and 1's!`);
+      }
       const result = await axios.post("http://localhost:9000/t9", {numbers});
       setT9Combinations(result.data);
+      setErrorMessage();
     };
 
     fetchData();
@@ -21,7 +27,8 @@ function App() {
       <form>
         <input type="number" onChange={(e) => setNumbers(e.target.value)} />
       </form>
-      {JSON.stringify(t9Combinations, null, 2)}
+      {numbers ? JSON.stringify(t9Combinations, null, 2) : errorMessage}
+      {numbers}
     </>
   );
 }
